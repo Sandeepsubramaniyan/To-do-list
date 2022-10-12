3b82293eee7b9eb527276382063071d97b7171b4
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-10-12 11:14:37 +0530

    create view using viewsets

--- todoapp/views.py
+++ todoapp/views.py
@@ -4,8 +4,9 @@ from .serializers import TodoSerializer
 from .models import Todo 
 # Create your views here.
 
-class TodoView(viewsets.ModelViewSet):
+class TodoView(viewsets.ModelViewSet):    
     serializer_class = TodoSerializer
-    querset=Todo.objects.all()
+    queryset=Todo.objects.all()
+    
     
     
